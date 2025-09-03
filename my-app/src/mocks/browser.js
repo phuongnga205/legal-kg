0a6c72cadf0f7ca5636389fa39aa5372051546a0
Le Nguyen My Ngoc <myngoclenguyen1612@gmail.com>
2025-09-04 00:38:26 +0900

    Delete my-app directory

--- my-app/src/mocks/browser.js
+++
@@ -1,5 +0,0 @@
-import { setupWorker } from 'msw/browser'; //tạo ra một service worker giả chạy trong browser
-import { handlers } from './handlers.js'; // danh sách các route mock đã định nghĩa trong file handlers.js
-//Worker này sẽ “chặn” mọi HTTP request trong trình duyệt, rồi kiểm tra xem có khớp với handler nào không 
-//→ nếu có thì trả dữ liệu giả, nếu không thì để request đi thật.
-export const worker = setupWorker(...handlers);
